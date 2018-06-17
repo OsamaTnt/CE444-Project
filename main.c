7546b246af1128c245dd52282bba503921bc9149
OsamaTnt <osamatantush@gmail.com>
2018-06-17 14:55:54 -0400

    001.Stable edition(Err: can't handle the spaces on the file)

--- main.c
+++ main.c
@@ -3,58 +3,60 @@
 #include <string.h>
 #include <ctype.h>
 
-int getFileSize(FILE *File)
+int getFileSize(FILE *srcFile)
 {
-    if(File) {fseek(File,0,SEEK_END); return ftell(File);}
+    if(srcFile) {fseek(srcFile,0,SEEK_END); return ftell(srcFile);}
     else {return 0;}
 }
 
-int getDataSize(FILE *File)
+int getDataSize(FILE *srcFile)
 {
     int Size=0;
 
-    if(File)
+    if(srcFile)
     {
-        char c;
-        fseek(File,0,SEEK_SET);
-        while((c=fgetc(File))!=EOF) { if(c=='\n'){Size++;} }
+       char c;
+       fseek(srcFile,0,SEEK_SET);
+       while((c=fgetc(srcFile))!=EOF) { if(c=='\n'){Size++;} }
     }
-
     return Size;
 }
 
-int *getLineSize(FILE *File,int dataSize)
+int *getLineSize(FILE *srcFile,int dataSize)
 {
     int i=0;
     int *LineSize=malloc(sizeof(int)*dataSize);
     for(i=0;i<dataSize;i++) {LineSize[i]=0;}
 
     char c;
-    fseek(File,0,SEEK_SET);
+    fseek(srcFile,0,SEEK_SET);
     i=0;
-    while((c=fgetc(File))!=EOF) {if(c=='\n'){LineSize[i++]++;} else{LineSize[i]++;} }
+    while((c=fgetc(srcFile))!=EOF) {if(c=='\n') {LineSize[i++]++;} else{LineSize[i]++;} }
 
     return LineSize;
 }
 
-char *getFileData(FILE *File,int dataSize)
+char *getFileData(FILE *srcFile)
 {
     char *buffer=NULL;
-    int *LineSize = getLineSize(File,getDataSize(File));
 
-    if(File)
+    if(srcFile)
     {
-        fseek(File,0,SEEK_SET);
-        buffer = malloc(sizeof(char)*dataSize);
-        char *temp=malloc(sizeof(char*));
-        int i=0;
-        while(fgets(temp,LineSize[i++]+1,File)) {strcat(buffer,temp);}
-    }return buffer;
+       int FileSize = getFileSize(srcFile);
+       int *LineSize = getLineSize(srcFile,getDataSize(srcFile));
+       fseek(srcFile,0,SEEK_SET);
+       buffer = malloc(sizeof(char)*FileSize);
+       char *temp=malloc(sizeof(char*));
+       int i=0;
+       while(fgets(temp,LineSize[i++]+1,srcFile)) {strcat(buffer,temp);}
+    }
+    return buffer;
 }
 
 void proc_Sort(char **Data,int dataSize)
 {
     int i=0,j=0,minIndex;
+    char *temp;
 
     for(i=0;i<dataSize-1;i++)
     {
@@ -62,12 +64,11 @@ void proc_Sort(char **Data,int dataSize)
         for(j=i+1;j<dataSize;j++)
         { if(strcmp(Data[j],Data[minIndex])<0) {minIndex=j;} }
 
-        //perform swap
         if(minIndex!=i)
         {
-            char *temp = Data[i];
-            Data[i] = Data[minIndex];
-            Data[minIndex] = temp;
+          temp = Data[i];
+          Data[i] = Data[minIndex];
+          Data[minIndex] = temp;
         }
     }
 }
@@ -75,7 +76,7 @@ void proc_Sort(char **Data,int dataSize)
 void Display(char **Data)
 {
     int i=0;
-    while(Data[i]!=NULL) {printf("%s\n",Data[i]);i++;}
+    while(Data[i]!=NULL) {printf("%s\n",Data[i++]);}
 }
 
 void storeToFile(FILE *File,char **Data,int dataSize)
@@ -85,13 +86,10 @@ void storeToFile(FILE *File,char **Data,int dataSize)
     if(!File){return;}
     else
     {
-        int i;
-        for(i=0;i<dataSize;i++)
-        {
-            fputs(Data[i],File);
-            fputc('\n',File);
-        }
-        fclose(File);
+       int i;
+       for(i=0;i<dataSize;i++)
+       {fputs(Data[i],File);fputc('\n',File);}
+       fclose(File);
     }
 }
 
@@ -101,11 +99,12 @@ void proc_BinarSearch(char *keyToFind,char**Data,int dataSize)
 
     while(isFound==0 && L<=U)
     {
-        M=L+(U-L)/2;
-        if(strcmp(keyToFind,Data[M])==0) {printf("%s is found at index #%d\n",keyToFind,M);isFound=1;return;}
-        else if(strcmp(keyToFind,Data[M])>0) {L=M+1;}
-        else {U=M-1;}
+       M=L+(U-L)/2;
+       if(strcmp(keyToFind,Data[M])==0) {printf("%s is found at index #%d\n",keyToFind,M);isFound=1;return;}
+       else if(strcmp(keyToFind,Data[M])>0) {L=M+1;}
+       else {U=M-1;}
     }
+
     if(isFound==0) {printf("%s was not found on the specified file\n",keyToFind);}
 }
 
@@ -119,21 +118,20 @@ int main(int argc,char* argv[])
     if(!srcFile) { printf("File COULD NOT BE FOUND\n"); return -1; }
     else
     {
-        char **Data = malloc(sizeof(char*)*getDataSize(srcFile));
-        i=0;
-        Data[i] = strtok(getFileData(srcFile,getFileSize(srcFile)),"\n");
-        while(Data[i++]!=NULL) {Data[i] = strtok(NULL,"\n");}
+       char **Data = malloc(sizeof(char*)*getDataSize(srcFile));
+       i=0;
+       Data[i] = strtok(getFileData(srcFile),"\n");
+       while(Data[i++]!=NULL) {Data[i] = strtok(NULL,"\n");}
 
-        proc_Sort(Data,getDataSize(srcFile));
-        storeToFile(srcFile,Data,getDataSize(srcFile));
+       proc_Sort(Data,getDataSize(srcFile));
+       storeToFile(srcFile,Data,getDataSize(srcFile));
 
-        //BinarySearch
-        char *keyToFind=malloc(sizeof(char*));
-        gets(keyToFind);
+       //BinarySearch
+       char *keyToFind=malloc(sizeof(char*));
+       gets(keyToFind);
 
-        proc_BinarSearch(keyToFind,Data,getDataSize(srcFile));
+       proc_BinarSearch(keyToFind,Data,getDataSize(srcFile));
 
     }
-
     return 0;
 }
